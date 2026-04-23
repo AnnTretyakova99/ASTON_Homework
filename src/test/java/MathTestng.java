c1d47cc6965fcad86c80a04ba35be5ace8589f27
AnnTretyakova99 <annpurr22pi@gmail.com>
2026-04-23 14:10:49 +0500

    +

--- src/test/java/MathTestng.java
+++ src/test/java/MathTestng.java
@@ -7,26 +7,26 @@ public class MathTestng {
 
     @Test
     public void testFactorial() {
-        Assert.assertEquals(math.factorial(5), 120);
+        Assert.assertEquals(math.factorial(4), 24);
         Assert.assertEquals(math.factorial(0), 1);
     }
 
     @Test
     public void testTriangleArea() {
-        Assert.assertEquals(math.triangleArea(5.0, 4.0), 10.0);
+        Assert.assertEquals(math.triangleArea(8.0, 3.0), 12.0);
     }
 
     @Test
     public void testArithmetic() {
-        Assert.assertEquals(math.add(7, 3), 10);
-        Assert.assertEquals(math.subtract(7, 3), 4);
-        Assert.assertEquals(math.multiply(7, 3), 21);
-        Assert.assertEquals(math.divide(6, 3), 2.0, 0.001);
+        Assert.assertEquals(math.add(15, 15), 30);
+        Assert.assertEquals(math.subtract(20, 30), -10);
+        Assert.assertEquals(math.multiply(5, 5), 25);
+        Assert.assertEquals(math.divide(10, 4), 2.5, 0.001);
     }
 
     @Test
     public void testCompare() {
-        Assert.assertEquals(math.compare(10, 5), "greater");
+        Assert.assertEquals(math.compare(100, 1), "greater");
         Assert.assertEquals(math.compare(5, 5), "equal");
     }
 }
