f37c8b0e496927210be9570e62039cd417584e1d
AnnTretyakova99 <annpurr22pi@gmail.com>
2026-04-23 12:48:06 +0500

    +

--- src/test/java/MathOperationTest.java
+++ src/test/java/MathOperationTest.java
@@ -1,4 +1,36 @@
+import org.homework.MathOperation;
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.*;
 
-public class MathOperationTest {
+class MathOperationTest {
+    MathOperation math = new MathOperation();
+
+    @Test
+    void testFactorial() {
+        assertEquals(120, math.factorial(5));
+        assertEquals(1, math.factorial(0));
+    }
+
+    @Test
+    void testTriangleArea() {
+        assertEquals(10.0, math.triangleArea(5.0, 4.0));
+    }
+
+    @Test
+    @DisplayName("Проверка арифметических действий")
+    void testArithmetic() {
+        assertAll(
+                () -> assertEquals(10, math.add(7,3)),
+                () -> assertEquals(4, math.subtract(7,3)),
+                () -> assertEquals(21, math.multiply(7,3)),
+                () -> assertEquals(2.0, math.divide(6, 3))
+        );
+    }
+
+    @Test
+    void testCompare(){
+        assertEquals("greater", math.compare(10, 5));
+        assertEquals("equal", math.compare(5, 5));
+    }
 }
