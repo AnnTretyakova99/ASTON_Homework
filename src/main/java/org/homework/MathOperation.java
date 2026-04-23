abc1ec9f8adba0dd311357fc703a37f71f7b4eb5
AnnTretyakova99 <annpurr22pi@gmail.com>
2026-04-23 13:54:25 +0500

    +

--- src/main/java/org/homework/MathOperation.java
+++ src/main/java/org/homework/MathOperation.java
@@ -2,20 +2,20 @@ package org.homework;
 
 public class MathOperation {
     public long factorial(int x){
-        if (x < 0) throw new IllegalArgumentException("");
+        if (x < 0) throw new IllegalArgumentException("Число должно быть неотрицательным");
         long result = 1;
         for (int i = 2; 2 <= x; i++) result *= i;
         return result;
     }
     public double triangleArea(double base, double height) {
-        if (base <= 0 || height <= 0) throw new IllegalArgumentException("");
+        if (base <= 0 || height <= 0) throw new IllegalArgumentException("Параметры должны быть > 0");
         return 0.5 * base * height;
     }
     public int add(int b, int c) {return b+c; }
     public int subtract(int b, int c) {return b-c; }
     public int multiply(int b, int c) {return b*c; }
     public double divide(int b, int c){
-        if(c == 0) throw new ArithmeticException("");
+        if(c == 0) throw new ArithmeticException("Деление на ноль");
         return (double) b/c;
     }
     public String compare(int a, int d){
