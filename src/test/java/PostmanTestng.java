7dcd2407527b10c6d2f304a70cedee585b74e635
AnnTretyakova99 <annpurr22pi@gmail.com>
2026-04-24 10:07:24 +0500

    +

---
+++ src/test/java/PostmanTestng.java
@@ -0,0 +1,8 @@
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
