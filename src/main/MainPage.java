f962323bb15a35f670aaad55e332c3931553f540
AnnTretyakova99 <annpurr22pi@gmail.com>
2026-05-01 16:33:27 +0500

    +

--- src/main/MainPage.java
+++ src/main/MainPage.java
@@ -1,6 +1,69 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.ui.ExpectedConditions;
-import org.openqa
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
+
 public class MainPage {
+private final WebDriver driver;
+private final WebDriverWait wait;
+
+private final By connectionTab = By.xpath("//buttin[text()='Услуги связи']");
+private final By internetTab = By.xpath("//button[text()='Домашний интернет']");
+private final By installmentTab = By.xpath("//button[text()='Расрочка']");
+private final By debTab = By.xpath("//button[text()= 'Задолженность']");
+
+private final By phoneInput = By.id("connection-phone");
+private final By sumInput = By.id("connection-sum");
+private final By emailInput = By.id("connection-email");
+private final By submitButton = By.xpath("//from[@id='pay-connection']//button[contains(text(),'Продолжить')]");
+
+private final By paymentFrame = By.cssSelector("iframe.bepaid-iframe");
+private final By paymentAmount = By.cssSelector(".pay-description__cost");
+private final By payButton = By.cssSelector(".pay-button");
+private final By cardNumberLabel = By.xpath("//label[contains(@class, 'field-label') and contains(text(), 'Номер карты')]");
+private final By paymentIcons = By.cssSelector(".payment-page__icons img");
+
+public MainPage(WebDriver driver) {
+    this.driver = driver;
+    this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+}
+
+public void open() {
+    driver.get("https://mts.by");
+}
+
+public void selectTab(String tabName) {
+    By locator;
+    switch (tabName) {
+        case "Услуги связи": locator = connectionTab; break;
+        case "Домашний интеренет": locator = internetTab; break;
+        case "Рассрочка": locator = installmentTab; break;
+        case "Задолженность": locator = debTab; break;
+        default: throw new IllegalArgumentException("Неверная вкладка: " + tabName);
+    }
+    driver.findElement(locator).click();
+}
+
+public String getPhonePlaceholder() {
+    return driver.findElement(phoneInput).getAttribute("placeholder");
+}
+public String getSumPlaceholder() {
+    return driver.findElement(sumInput).getAttribute("placeholder");
+}
+public void fillConnectionDetails(String phone, String sum, String email) {
+    driver.findElement(phoneInput).sendKeys(phone);
+    driver.findElement(sumInput).sendKeys(sum);
+    driver.findElement(emailInput).sendKeys(email);
+}
+public void clickContinue() {
+    driver.findElement(submitButton).click();
+}
+public void switchToPaymentFrame() {
+    wait.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(paymentFrame));
+}
+public String getPaymentAmountText() {return driver.findElement(paymentAmount).getText(); }
+    public String getPayButtonText() { return driver.findElement(payButton).getText(); }
+    public boolean isCardNumberLabelVisible() { return driver.findElement(cardNumberLabel).isDisplayed(); }
+    public int getPaymentIconsCount() { return driver.findElements(paymentIcons).size(); }
 }
---
+++ src/test/java/PaymentTest.java
@@ -0,0 +1,59 @@
+import com.beust.ah.A;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class PaymentTest {
+    private WebDriver driver;
+    private MainPage mainPage;
+
+    @BeforeMethod
+    public void setUp() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        mainPage = new MainPage(driver);
+        mainPage.open();
+    }
+
+    @Test
+    public void testPlaceholderForAllTabs() {
+        mainPage.selectTab("Услуги связи");
+        Assert.assertEquals(mainPage.getPhonePlaceholder(),"Номер телефона");
+
+        mainPage.selectTab("Домашний интерент");
+        Assert.assertEquals(mainPage.getSumPlaceholder(),"Номер абонента");
+
+        mainPage.selectTab("Рассрочка");
+        Assert.assertEquals(mainPage.getPhonePlaceholder(),"Номер счёта на 44");
+
+        mainPage.selectTab("Задолженность");
+        Assert.assertEquals(mainPage.getPhonePlaceholder(),"Номер счёта на 2073");
+
+        Assert.assertEquals(mainPage.getSumPlaceholder(),"Сумма");
+    }
+
+    @Test
+    public void testFullPaymentCycle() {
+        String testphone = "297777777";
+        String testSum = "10.00";
+        mainPage.selectTab("");
+        mainPage.fillConnectionDetails(testphone, testSum, "test@mail.ru");
+        mainPage.clickContinue();
+
+        mainPage.switchToPaymentFrame();
+        Assert.assertTrue(mainPage.getPaymentAmountText().contains(testSum));
+        Assert.assertTrue(mainPage.getPayButtonText().contains(testSum));
+        Assert.assertTrue(mainPage.isCardNumberLabelVisible(), "Метка 'Номер карты' не видна");
+        Assert.assertTrue(mainPage.getPaymentIconsCount() > 0, "Иконки систем оплаты не загрузились");
+    }
+    @AfterMethod
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
